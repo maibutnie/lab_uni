a7c72a0c8f7c947b10eff71c8044f5005ec09fd6
russianbillie <56802951+russianbillie@users.noreply.github.com>
2020-12-24 00:39:40 +0300

    Create library.h

---
+++ lab_8/library.h
@@ -0,0 +1,22 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#define size 1000
+
+void remove_spaces(char* str, char* str2);
+char* replace(char* s, char* suf1, char* suf2);
+char* ftstrdel(char* s, int count);
+char* ftstrins(char* s, char* sub_s);
+char* ftstrcat(char* s1, char* s2);
+size_t ftstrlen(const char* str);
+void free_string(char* s);
+void output_string(char* s);
+void input_string(char* s);
+char* ftstrstr(char* s1, const char* s2);
+char* ftstrcpy(char* dest, const char* src);
+void remove_spaces(char* str, char* str2);
+char* make_place();
+
+#endif
