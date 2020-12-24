ce120f25e4a00186768ee3384f9698c7886819ea
russianbillie <56802951+russianbillie@users.noreply.github.com>
2020-12-24 18:32:18 +0300

    Create main.c

---
+++ lab_9/9_1/main.c
@@ -0,0 +1,19 @@
+#include "library.h"
+
+int main()
+{
+    char* str = make_place(SIZE);
+    input_string(str);
+    remove_dots(str);
+    remove_commas(str);
+    without_spaces(str);
+    char** p_words;
+    split(&p_words, str, " ");
+    int count = words_count(str, " ");
+    int dict[count];
+    creating_dict(p_words, dict, count);
+    output_dict(p_words, dict, count);
+    free_string(str);
+    free_mas(p_words);
+    return 0;
+}
