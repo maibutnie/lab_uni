d4514c3c811f63742b73a59c93460d8f8fc0b35e
russianbillie <56802951+russianbillie@users.noreply.github.com>
2020-12-24 18:33:06 +0300

    Create library.h

---
+++ lab_9/9_1/library.h
@@ -0,0 +1,22 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 400
+
+char* make_place(int size);
+void input_string(char* s);
+void output_string(char* s);
+void free_string(char* s);
+void without_spaces(char* s);
+void remove_commas(char* s);
+void remove_dots(char* s);
+void find_word(char* string, char* word);
+void split(char*** p_words, char* str, const char* symb);
+void creating_dict(char** p_words, int* dict, int count);
+void output_dict(char** p_words, int* dict, int count);
+
+#endif
